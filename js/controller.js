b0136c5a65088195dbbe65048d8cc3bb6ce211f3
tony.zhu <tony.zhu@irdeto.com>
2017-02-12 23:46:17 +0800

    update

--- js/controller.js
+++ js/controller.js
@@ -119,4 +119,61 @@ $(document).ready(function(){
       $('.navbar').removeClass('navbar-fixed-top');
     }
   });
+
+
+
+    var imgUrl = "http://www.tz-songshui.com/images/tong2.png";  //图片LOGO注意必须是绝对路径
+    var lineLink = "http://www.tz-songshui.com";   //网站网址，必须是绝对路径
+    var descContent = '同洲源水站促销开始了，喝健康水，结通州缘。'; //分享给朋友或朋友圈时的文字简介
+    var shareTitle = '同洲源送水站';  //分享title
+    var appid = ''; //apiID，可留空
+
+    function shareFriend() {
+        WeixinJSBridge.invoke('sendAppMessage',{
+            "appid": appid,
+            "img_url": imgUrl,
+            "img_width": "200",
+            "img_height": "200",
+            "link": lineLink,
+            "desc": descContent,
+            "title": shareTitle
+        }, function(res) {
+            //_report('send_msg', res.err_msg);
+        })
+    }
+    function shareTimeline() {
+        WeixinJSBridge.invoke('shareTimeline',{
+            "img_url": imgUrl,
+            "img_width": "200",
+            "img_height": "200",
+            "link": lineLink,
+            "desc": descContent,
+            "title": shareTitle
+        }, function(res) {
+            //_report('timeline', res.err_msg);
+        });
+    }
+    function shareWeibo() {
+        WeixinJSBridge.invoke('shareWeibo',{
+            "content": descContent,
+            "url": lineLink,
+        }, function(res) {
+            //_report('weibo', res.err_msg);
+        });
+    }
+    // 当微信内置浏览器完成内部初始化后会触发WeixinJSBridgeReady事件。
+    document.addEventListener('WeixinJSBridgeReady', function onBridgeReady() {
+        // 发送给好友
+        WeixinJSBridge.on('menu:share:appmessage', function(argv){
+            shareFriend();
+        });
+        // 分享到朋友圈
+        WeixinJSBridge.on('menu:share:timeline', function(argv){
+            shareTimeline();
+        });
+        // 分享到微博
+        WeixinJSBridge.on('menu:share:weibo', function(argv){
+            shareWeibo();
+        });
+    }, false);
 });
